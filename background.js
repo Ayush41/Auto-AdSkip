c624d3cee581402d5961bfc381668eb1a1164db8
ayush41 <anonymousking41681@gmail.com>
2025-12-16 19:55:02 +0530

    update

--- background.js
+++ background.js
@@ -1,17 +1,33 @@
 // Clicking the extension icon toggles it ON/OFF.
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ enabled: true });
-});
-
-chrome.action.onClicked.addListener(() => {
-  chrome.storage.sync.get("enabled", (data) => {
-    const newState = !data.enabled;
-    chrome.storage.sync.set({ enabled: newState });
-
-    chrome.action.setBadgeText({
-      text: newState ? "ON" : "OFF"
-    });
-  });
+  chrome.action.setBadgeText({ text: "ON" });
 });
 
 console.log("Auto-AdSkip extension is running.");
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ enabled: true });
+// });
+
+// chrome.action.onClicked.addListener(() => {
+//   chrome.storage.sync.get("enabled", (data) => {
+//     const newState = !data.enabled;
+//     chrome.storage.sync.set({ enabled: newState });
+
+//     chrome.action.setBadgeText({
+//       text: newState ? "ON" : "OFF"
+//     });
+//   });
+// });
+
+// console.log("Auto-AdSkip extension is running.");
+
+
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ enabled: true });
+
+//   chrome.action.setBadgeText({ text: "ON" });
+// });
